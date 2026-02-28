824ed11c84fb5b8f7e78a8580c02dfd2a382b7be
carraguti-pro <carraguti@gmail.com>
2026-02-28 20:59:22 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+/* Simulador Hipotecario Pro - Service Worker (offline) */
+const CACHE_NAME = "simulador-pro-premium-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./assets/logo.jpg",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png",
+  "./assets/apple-touch-icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).catch(() => caches.match("./index.html")))
+  );
+});
