3d5aa65357156aead227fdb56d75bbc9823a7d89
carraguti-pro <carraguti@gmail.com>
2026-03-01 20:11:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simulador Hipotecario Pro - Service Worker (offline) */
-const CACHE_NAME = "simulador-pro-premium-v16";
+const CACHE_NAME = "simulador-pro-premium-v17";
 const ASSETS = [
   "./",
   "./index.html",
